1514c4ec5117010a121bf5b23956e5949e553f29
khayss <forendals@gmail.com>
2024-10-04 09:53:36 +0100

    feat: add config for lisk sepolia and deploy on lisk sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,19 +2,41 @@ import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
-const ETHER_SCAN_API_KEY = vars.get("ETHER_SCAN_API_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
+    },
+    lisk: {
+      url: "",
+      accounts: [PRIVATE_KEY],
+    },
+    liskSepolia: {
+      accounts: [PRIVATE_KEY],
+      url: "https://rpc.sepolia-api.lisk.com",
     },
   },
   etherscan: {
-    apiKey: ETHER_SCAN_API_KEY,
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+
+      "lisk-sepolia": "empty",
+    },
+    customChains: [
+      {
+        network: "lisk-sepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
   },
 };
 
